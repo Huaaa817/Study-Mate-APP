0d9d0563cc7cd91074e3a3f1aee63272b302a310
Yuhung <irissu0817@gapp.nthu.edu.tw>
2025-05-03 20:33:31 +0800

    add image_study_mate.ts to index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import { onCallGenkit } from 'firebase-functions/https';
 // TODO: export your functions
 import './flow/customRecipe';
 import './flow/retrieveRecipe';
-import { customRecipeFlow } from './flow/customRecipe';
-export const customRecipe = onCallGenkit(customRecipeFlow);
-import { retrieveRecipeFlow } from "./flow/retrieveRecipe";
-export const retrieveRecipe = onCallGenkit(retrieveRecipeFlow);
+import './flow/image_study_mate';
+import { studyMateImageFlow } from "./flow/image_study_mate";
+export const studyMateImage = onCallGenkit(studyMateImageFlow);
+
