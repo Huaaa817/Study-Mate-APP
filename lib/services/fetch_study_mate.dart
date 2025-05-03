0c84023a6cd3944457de1f83e63a89ebfacc5dc5
Yuhung <irissu0817@gapp.nthu.edu.tw>
2025-05-03 20:35:10 +0800

    add fetch_study_mate.dart

--- lib/services/fetch_study_mate.dart
+++ lib/services/fetch_study_mate.dart
@@ -1,3 +1,48 @@
 import 'package:cloud_functions/cloud_functions.dart';
 
 // TODO: calling your Flow
+Future<Map<String, dynamic>> fetchStudyMateImage(
+  String hairLength,
+  String hairColor,
+  String hairStyled,
+  String decoration,
+  String skinTone,
+  String smileFeelings,
+  String eyeFeeling,
+  String personality1,
+  String personality2,
+  String userSuggestion,
+) async {
+  try {
+    final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable(
+      // 'customRecipe',
+      'studyMateImage',
+    );
+
+    final response = await callable.call({
+      'imageSetting': {
+        'hairLength': hairLength,
+        'hairColor': hairColor,
+        'hairStyled': hairStyled,
+        'decoration': decoration,
+        'skinTone': skinTone,
+        'smileFeelings': smileFeelings,
+        'eyeFeeling': eyeFeeling,
+        'personality1': personality1,
+        'personality2': personality2,
+      },
+      'otherDescription': userSuggestion,
+    });
+
+    final Map<String, dynamic> data = Map<String, dynamic>.from(
+      response.data as Map,
+    );
+    //final customRecipe = Map<String, dynamic>.from(data["customRecipe"]);
+
+    //return customRecipe;
+    return data;
+  } catch (e) {
+    print("Error calling studymate image: $e");
+    throw Exception("Failed to fetch studymate image");
+  }
+}
